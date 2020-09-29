ac6a2cbc1b41c3b7df8c9e3dd2be68c94b0ec798
mfgonzale <69164101+mfgonzale@users.noreply.github.com>
2020-09-28 23:05:09 -0300

    Last commit

--- reticulado.py
+++ reticulado.py
@@ -1,69 +1,79 @@
 import numpy as np
 
 class Reticulado(object):
-    """Define un reticulado"""
+	"""Define un reticulado"""
 
-    def __init__(self):
-        super(Reticulado, self).__init__()
+	def __init__(self):
+		super(Reticulado, self).__init__()
+		
+		self.xyz = np.zeros((0,3), dtype=np.double)
+		self.Nnodos = 0
+		self.barras = []
+		self.cargas = {}
+		self.restricciones = {}
+        
 
-        self.xyz = np.zeros((0,3), dtype=np.double)
-        self.Nnodos = 0
-        self.barras = []
-        self.cargas = {}
-        self.restricciones = {}
-    def agregar_nodo(self, x, y, z=0):
-        #Cambiar Tamaño
-        self.xyz.resize((self.Nnodos+1,3))
-        self.xyz[self.Nnodos,:] = [x,y,z]
-        self.Nnodos +=1
-        return
+	def agregar_nodo(self, x, y, z=0):
+		#Cambiar Tamaño
+		self.xyz.resize((self.Nnodos+1,3))
+		self.xyz[self.Nnodos,:] = [x,y,z]
+		self.Nnodos +=1
+		return
+		
+	def agregar_barra(self, barra):
+		self.barras.append(barra)
+		return None
 
-    def agregar_barra(self, barra):
-	    self.barras.append(barra)
-	    return
+	def obtener_coordenada_nodal(self, n): 
+		return self.xyz[n]
 
-    def obtener_coordenada_nodal(self, n):
-        if n>= self.Nnodos:
-            return
-        return self.xyz[n,:]
-    def calcular_peso_total(self):
-        w=0
-        for i in self.barras:
-            w += i.calcular_peso(self)
-        return w
-    def obtener_nodos(self):
-        """Implementar"""
+	def calcular_peso_total(self):
+		peso=0
+		for barra in self.barras:
+			peso += barra.calcular_peso(self)
+		return peso
 
-        return
+	def obtener_nodos(self):
+		return self.xyz
 
-    def obtener_barras(self):
-        """Implementar"""
-        return
-    def agregar_restriccion(self, nodo, gdl, valor=0.0):
-        """Implementar"""
-        return
+	def obtener_barras(self):
+		return self.barras
 
-    def agregar_fuerza(self, nodo, gdl, valor):
-        """Implementar"""
-        return
+	def agregar_restriccion(self, nodo, gdl, valor=0.0):
+		"""Implementar"""
+		if nodo in self.restricciones:
+			self.restricciones[nodo].append([gdl,valor])
+		else:
+			self.restricciones[nodo] = [[gdl,valor]]
+		pass
 
-    def ensamblar_sistema(self):
-        """Implementar"""
-        return
+	def agregar_fuerza(self, nodo, gdl, valor):
+		"""Implementar"""
+		return
 
-    def resolver_sistema(self):
-        """Implementar"""
-        return
-    def recuperar_fuerzas(self):
-        """Implementar"""
-        return
+	def ensamblar_sistema(self):
+		"""Implementar"""
+		return
+
+	def resolver_sistema(self):
+		"""Implementar"""
+		return
+
+	def recuperar_fuerzas(self):
+		"""Implementar"""
+		return
     
 	def _str_(self):
 		s = "reticulado \n"
 		s += "Nodos \n"
-		s += f"{self.xyz}"
+		for n in range(self.Nnodos):
+			s += f"{n} : ({self.xyz[n,0]}, {self.xyz[n,1]}, {self.xyz[n,2]})\n"
+		s += "\n\n"
+	
 		s += "Barras \n"
-		s += f"{self.barras}"
+		for i, b in enumerate(self.barras):
+			n = b.obtener_conectividad()
+			s += f"{i} : [ {n[0]} {n[1]} ] \n"
 		return s
 
-    
+
